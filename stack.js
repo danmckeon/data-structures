73dbd768f455e8cd5a4c548ac9050b5cb46ad58f
Dan McKeon <dpmckeon@gmail.com>
2017-07-08 13:49:57 -0700

    implement stack with array

--- stack.js
+++ stack.js
@@ -1,55 +1,90 @@
 var Stack = function(init){
-  let data = {};
-  let count;
-  if (init) {
-    let i;
-    for(i = 0; i < init.length; i++) {
-      data[i] = init[i];
-    }
-    count = i;
-  } else {
-    count = 0;
-  }
+  // ARRAY IMPLEMENTATION
 
+  let data = (init) ? init : [];
 
   this.print = function() {
-    if (count === 0) {
-      console.log('00000 Stack is Empty 00000')
-    } else {
-      console.log('***** Top of Stack *****');
-      for (let i = count - 1; i >= 0; i--) {
-        console.log(data[i]);
+    if (data.length === 0) {
+        console.log('00000 Stack is Empty 00000')
+      } else {
+        console.log('***** Top of Stack *****');
+        for (let i = data.length - 1; i >= 0; i--) {
+          console.log(data[i]);
+        }
+        console.log('##### Bottom of Stack #####')
       }
-      console.log('##### Bottom of Stack #####')
-    }
   }
 
   this.push = function(newElement) {
-    data[count] = newElement;
-    count++;
+    data.push(newElement);
     return newElement;
   }
 
   this.pop = function() {
-    if(count === 0) {
-      return undefined;
-    }
-    let popped = data[count - 1];
-    delete data[count - 1];
-    count--;
-    return popped;
+    return data.splice(data.length - 1, 1)[0];
   }
 
   this.peek = function() {
-    if(count === 0) {
-      return undefined;
-    }
-    return data[count - 1];
+    return data[data.length - 1];
   }
 
   this.size = function() {
-    return count;
+    return data.length;
   }
+
+  // OBJECT IMPLEMENTATION
+  //
+  // let data = {};
+  // let count;
+  // if (init) {
+  //   let i;
+  //   for(i = 0; i < init.length; i++) {
+  //     data[i] = init[i];
+  //   }
+  //   count = i;
+  // } else {
+  //   count = 0;
+  // }
+  //
+  //
+  // this.print = function() {
+  //   if (count === 0) {
+  //     console.log('00000 Stack is Empty 00000')
+  //   } else {
+  //     console.log('***** Top of Stack *****');
+  //     for (let i = count - 1; i >= 0; i--) {
+  //       console.log(data[i]);
+  //     }
+  //     console.log('##### Bottom of Stack #####')
+  //   }
+  // }
+  //
+  // this.push = function(newElement) {
+  //   data[count] = newElement;
+  //   count++;
+  //   return newElement;
+  // }
+  //
+  // this.pop = function() {
+  //   if(count === 0) {
+  //     return undefined;
+  //   }
+  //   let popped = data[count - 1];
+  //   delete data[count - 1];
+  //   count--;
+  //   return popped;
+  // }
+  //
+  // this.peek = function() {
+  //   if(count === 0) {
+  //     return undefined;
+  //   }
+  //   return data[count - 1];
+  // }
+  //
+  // this.size = function() {
+  //   return count;
+  // }
 
 }
 
@@ -63,7 +98,8 @@ let myPeeked = myStack.peek();
 console.log(myPeeked)
 myStack.print();
 console.log(myStack.size());
-myStack.pop();
+let myPopped = myStack.pop();
+console.log(myPopped);
 myStack.pop();
 myStack.pop();
 myStack.print();
