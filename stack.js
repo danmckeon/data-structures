6ef20f68117a021c6e6956026707979000094fd9
Dan McKeon <dpmckeon@gmail.com>
2017-07-08 11:41:17 -0700

    refactor stack object implementation to es6

--- stack.js
+++ stack.js
@@ -1,7 +1,8 @@
 var Stack = function(init){
   this.storage = {};
   if (init) {
-    for(var i = 0; i < init.length; i++) {
+    let i;
+    for(i = 0; i < init.length; i++) {
       this.storage[i] = init[i];
     }
     this.length = i;
@@ -20,7 +21,7 @@ var Stack = function(init){
     if(this.length === 0) {
       return undefined;
     }
-    var popped = this.storage[this.length - 1];
+    let popped = this.storage[this.length - 1];
     delete this.storage[this.length - 1];
     this.length--;
     return popped;
@@ -35,14 +36,14 @@ var Stack = function(init){
 
 }
 
-// var myStack = new Stack([1,2,3]);
-// console.log(JSON.stringify(myStack.storage));
-// console.log(myStack.length);
-// myStack.push(5);
-// // console.log(myVar)
-// console.log(JSON.stringify(myStack.storage));
-// console.log(myStack.length);
-// var myPeeked = myStack.peek();
-// console.log(myPeeked)
-// console.log(JSON.stringify(myStack.storage));
-// console.log(myStack.length);
+let myStack = new Stack([1,2,3]);
+console.log(JSON.stringify(myStack.storage));
+console.log(myStack.length);
+myStack.push(5);
+// console.log(myVar)
+console.log(JSON.stringify(myStack.storage));
+console.log(myStack.length);
+let myPeeked = myStack.peek();
+console.log(myPeeked)
+console.log(JSON.stringify(myStack.storage));
+console.log(myStack.length);
